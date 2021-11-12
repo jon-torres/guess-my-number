87ca43c217fcc4f8ae3e5ba797aa2d6fddf6970e
Jon Torres <jonathantorres10@gmail.com>
2021-11-12 10:52:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+const updateNumber = function (value, ...values) {
+  document.querySelector(".number").textContent = value;
+  document.querySelector(".number").style.width = values;
+  document.querySelector(".number").textContent = values;
+};
+
+const updateScore = function (newScoreValue) {
+  document.querySelector(".score").textContent = newScoreValue;
+};
+
+const updateBackground = function (newBackground) {
+  document.querySelector("body").style.backgroundColor = newBackground;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  //   No input or not a number
+  if (!guess) {
+    displayMessage("Please, guess a number 🔢!");
+
+    //   Player wins
+  } else if (guess === secretNumber) {
+    if (score > highscore) {
+      highscore = score;
+    }
+    confetti.start();
+    displayMessage("🏆 You Won! That's the correct Number! 🏆");
+    document.querySelector(".highscore").textContent = highscore;
+
+    updateNumber("30rem", secretNumber);
+    updateBackground("#60b347");
+
+    // wrong guess
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "Too high! 📈" : "Too low! 📉");
+      score--;
+      updateScore(score);
+    } else {
+      displayMessage("💥 You lost the game 😭. Try again! 💪");
+      updateScore(0);
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  confetti.stop();
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage("Start guessing...");
+  document.querySelector(".guess").value = "";
+  updateScore(0);
+  updateNumber("15rem", "?");
+  updateBackground("#222");
+});
